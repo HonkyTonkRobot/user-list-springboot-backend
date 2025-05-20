254552989f58a40b99ec82e4ec5eff922c355e45
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-05-20 15:10:53 +1200

    create post and get routes in UserController.java

---
+++ springboot-java-fullstack/src/main/java/com/honkytonkrobot/springboot_java_fullstack/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.honkytonkrobot.springboot_java_fullstack.controller;
+
+import com.honkytonkrobot.springboot_java_fullstack.model.User;
+import com.honkytonkrobot.springboot_java_fullstack.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
