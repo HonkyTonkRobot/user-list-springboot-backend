198154cd9fef9578016cedb9086e8616b202c531
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-05-21 14:42:56 +1200

    Full-stack-app ready to make live (#1)
    * add docker-compose.yml file with starter config
    
    * add ai acknowledgement of starter config
    
    * add www/index.php test file
    
    * add phpMyAdmin for GUI
    
    * add springboot intitializr files
    
    * add acknowledgement to README.md
    
    * add User.java with Getter and setter for user data
    
    * add UserRepository and connection JpaRepository
    
    * change MYSQL_DATABASE name
    
    * add springboot to docker-compose
    
    * setup springboot application.properties
    
    * add starter Dockerfile
    
    * comment springboot while running locally
    
    * create post and get routes in UserController.java
    
    * add crossorigin to UserController
    
    * add get user by Id server route
    
    * add put server side route
    
    * fix: springboot incrementing by 50 on each server run
    
    * feat: write springboot delete route

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+**acknowledgements** This project is a code along with [Arjun Gautam](https://www.youtube.com/watch?v=TW5PD_TJMXo&list=PL1oBBulPlvs84AmRmT-_3dGz4KHYuINsj)
 # Spring boot, java, react fullstack app practice
 I've got an interview with a company that uses this tech stack. So taking this weeks project as an opportunity to familiarize myself with this tech stack.
 
---
+++ springboot-java-fullstack/src/main/java/com/honkytonkrobot/springboot_java_fullstack/SpringbootJavaFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.honkytonkrobot.springboot_java_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJavaFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJavaFullstackApplication.class, args);
+	}
+
+}
